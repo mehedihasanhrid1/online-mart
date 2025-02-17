dd449fdc1f3088caa47534143f78ea2fea7a1402
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2025-02-17 16:36:04 +0600

    copy

---
+++ script.js
@@ -0,0 +1,20 @@
+const copyDiscountCoupon = document.getElementById('discount-coupon');
+copyDiscountCoupon.addEventListener('click', function() {
+  const discountText = copyDiscountCoupon.innerText;
+  const textarea = document.createElement('textarea');
+  textarea.value = discountText;
+  document.body.appendChild(textarea);
+  
+  textarea.select();
+  textarea.setSelectionRange(0, 99999); 
+
+  document.execCommand('copy');
+
+  document.body.removeChild(textarea);
+  
+  copyDiscountCoupon.textContent = 'Copied!';
+  
+  setTimeout(() => {
+    copyDiscountCoupon.textContent = discountText;
+  }, 900);
+});
